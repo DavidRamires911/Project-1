e809faaa5a6de0b7d14b04d050ea35c801f6c202
DavidRamires911 <124219071+DavidRamires911@users.noreply.github.com>
2023-03-06 14:29:43 +0000

    traps Optimization

--- javacript/game.js
+++ javacript/game.js
@@ -6,10 +6,6 @@
   -Set timout for traps (cooldown)
   -Bigger rat that can hit 2 traps 
   -Sound, random rat sound and dips                            */
-  
- 
- 
-
 
 class Player {
   constructor() {
@@ -37,22 +33,22 @@ class Player {
   moveLeft() {
     this.positionX--;
     this.domElement.style.left = this.positionX + "vw";
-    console.log("position...", this.positionX);
+    //console.log("position...", this.positionX);
   }
   moveRight() {
     this.positionX++;
     this.domElement.style.left = this.positionX + "vw";
-    console.log("position...", this.positionX);
+    //console.log("position...", this.positionX);
   }
   moveUp() {
     this.positionY++;
     this.domElement.style.bottom = this.positionY + "vh";
-    console.log("position...", this.positionY);
+    //console.log("position...", this.positionY);
   }
   moveDown() {
     this.positionY--;
     this.domElement.style.bottom = this.positionY + "vh";
-    console.log("position...", this.positionY);
+    //console.log("position...", this.positionY);
   }
   // //   moveDiagonalUpRight(){
   // //     this.positionX++;
@@ -73,17 +69,16 @@ class Rats {
     this.domElement = null;
     this.createDomElement();
     this.randomSpawn();
-    
   }
   createDomElement() {
-    this.domElement = document.createElement("div");
+    this.ratElement = document.createElement("div");
 
-    this.domElement.className = "rats";
-    this.domElement.style.width = this.width + "vw";
-    this.domElement.style.height = this.height + "vh";
+    this.ratElement.className = "rats";
+    this.ratElement.style.width = this.width + "vw";
+    this.ratElement.style.height = this.height + "vh";
 
     const mapElm = document.getElementById("map");
-    mapElm.appendChild(this.domElement);
+    mapElm.appendChild(this.ratElement);
   }
   moveDown() {
     this.positionY--;
@@ -92,40 +87,66 @@ class Rats {
 
   ////////////////
   follow(player) {
-
-     ///distance between player and obstacle 
+    ///distance between player and obstacle
     const distanceX = player.positionX - this.positionX;
     const distanceY = player.positionY - this.positionY;
     const dist = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-    
+
     this.speed = 1; //////Need to change over time or score????
     const vx = (distanceX / dist) * this.speed;
     const vy = (distanceY / dist) * this.speed;
     this.positionX += vx;
     this.positionY += vy;
 
-    this.domElement.style.left = this.positionX + "vw";
-    this.domElement.style.bottom = this.positionY + "vh";
+    this.ratElement.style.left = this.positionX + "vw";
+    this.ratElement.style.bottom = this.positionY + "vh";
   }
 
-
-
-/////////////////////Work in progress spawn inside the map, add with this.positionY + vw this.positionX + vh
+  /////////////////////Work in progress spawn inside the map, add with this.positionY + vw this.positionX + vh
   randomSpawn() {
-   
-    const mapWidth = 70; 
+    const mapWidth = 70;
+    const mapHeight = 90;
 
-  
-  this.positionX = Math.floor(Math.random() * (mapWidth + 1));
+    this.positionX = Math.floor(Math.random() * (mapWidth + 1));
+    this.positionY = Math.floor(Math.random() * (mapHeight + 1));
 
-  // update the position of the rat's DOM element
+    // update the position of the rat's DOM element
+    this.ratElement.style.left = this.positionX + "vw";
+    this.ratElement.style.bottom = this.positionY + "vh";
+  }
+}
+class Traps {
+  constructor(positionX, positionY) {
+    this.width = 3;
+    this.height = 6;
+    this.positionX = positionX
+    this.positionY = positionY
+    this.domElement = null;
+    this.createDomElement()
+
+  }
+  createDomElement(){
+    this.domElement = document.createElement("div")
+    this.domElement.id = "traps"
+
+    this.domElement.style.width = this.width + "vw";
+  this.domElement.style.height = this.height + "vh";
+  this.domElement.style.bottom = this.positionY + "vh";
   this.domElement.style.left = this.positionX + "vw";
-}
-  
-  
-}
 
-class Trap1{
+
+  const mapElm = document.getElementById("map");
+  mapElm.appendChild(this.domElement);
+
+  }
+   removeRat(ratInstance){
+    ratArray.splice(index, 1);
+
+        ratInstance.ratElement.remove()
+
+   }
+}
+/*class Trap1{
 constructor(){
   this.positionX =15;
     this.positionY = 70;
@@ -217,15 +238,17 @@ class Trap2{
         mapElm.appendChild(this.domElement);
        }
       }
-
+*/
 const player = new Player();
 
 const ratArray = [];
 
-const trap1 = new Trap1()
-const trap2 = new Trap2()
-const trap3= new Trap3()
-const trap4= new Trap4()
+const trap1 = new Traps(15,70);
+const trap2 = new Traps(45,55);
+const trap3 = new Traps(45,5);
+const trap4 = new Traps(15,20);
+
+
 
 ///// Movement keys
 
@@ -256,22 +279,47 @@ setInterval(() => {
 //move rats
 setInterval(() => {
   ratArray.forEach((ratInstance) => {
-    this.ratInstance = ratInstance
+    this.ratInstance = ratInstance;
     this.ratInstance.follow(player);
-    
-    
+
     if (
       player.positionX < this.ratInstance.positionX + this.ratInstance.width &&
       player.positionX + player.width > this.ratInstance.positionX &&
       player.positionY < this.ratInstance.positionY + this.ratInstance.height &&
       player.height + player.positionY > this.ratInstance.positionY
-      ) {
-        console.log("collision");
-        }
-        
-      });
+    ) {
+      const index = ratArray.indexOf(ratInstance);
+      if (index === 1 || 2 || 3 || 4) {
+        ratArray.splice(index, 1);
 
+        ratInstance.ratElement.remove();
+      }
+
+      console.log("collision");
+    }
+  });
 }, 200);
 
+/*setTimeout(()=>{
+  trapArray.forEach((trapInstance)=>{
+    
 
-  
+    if (trapInstance.positionX < this.ratInstance.positionX + this.ratInstance.width &&
+      trapInstance.positionX +trapInstance.width > this.ratInstance.positionX &&
+      trapInstance.positionY < this.ratInstance.positionY + this.ratInstance.height &&
+      trapInstance.height + trapInstance.positionY > this.ratInstance.positionY
+      ) {
+        const index = ratArray.indexOf(this.ratInstance)
+        if (index === 1||2||3||4) {
+          ratArray.splice(index, 1);
+          
+          this.trapInstance.this.ratInstance.ratElement.remove()
+          }
+          console.log("TRAP")
+           }
+           
+          })
+
+},1000)*/
+
+////
